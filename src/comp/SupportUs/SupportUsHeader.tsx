1c4a39fbc2739758ee06b2ccd5a6fa726228821e
Abdalrahman15 <abdalrahman200324@gmail.com>
2025-12-18 17:21:15 +0200

    ..

--- src/comp/SupportUs/SupportUsHeader.tsx
+++ src/comp/SupportUs/SupportUsHeader.tsx
@@ -5,7 +5,7 @@ export default function SupportUsHeader() {
   return<>
 
   <div className={`${Style.supportUsHeaderBg} h-screen md:object-cover object-cover object-right relative overflow-hidden flex  items-center`}>
-    <div className='md:ms-52 ms-20 z-1'>
+    <div className='md:ms-52 ms-10 z-1'>
         <p className='text-white font-bold md:text-[40px] text-[20px]'>Seeking Truth & Transparency</p>
         <p className=' font-bold md:text-[30px] text-[15px]'>Made by al Quarisma</p>
         <p className='text-white  md:text-[20px] text-[10px]'>WHAT CAN WE HELP YOU WITH ?</p>
